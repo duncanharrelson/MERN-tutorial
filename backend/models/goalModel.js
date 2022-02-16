9591fd335791290870677e9712e4196864024f7a
Duncan Harrelson <orensjet@gmail.com>
2022-02-16 14:22:11 -0600

    added auth middleware and protected routes, jwt, password hashing

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
-const goalSchema = mongoose.Schema({
+const goalSchema = mongoose.Schema(
+{
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     text: {
         type: String,
         requied: [true, 'Please add a text value']
     }
-}, {
+}, 
+{
     timestamps: true
 })
 
